9b53eab33104ecde4dfd98e3d7e7b1c20e856af0
Breno Souza <brenojcs10@gmail.com>
2024-04-04 02:02:41 -0300

    Update unite.js

--- curso unite/unite.js
+++ curso unite/unite.js
@@ -1,10 +1,3 @@
-const participante = {
-    nome: "Maik Brito",
-    email: "maikbrito@gmail.com",
-    dtataInscricao: new Date (2024, 2, 22, 19, 20 ),
-    dataCheckIn: new Date (2024, 2, 25, 22, 0)
-};
-
 // array
 let participantes = [
     {
